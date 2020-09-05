9bf63997c77a12ccd279e59b435a73b97ee404ca
Hsjoshi96 <39207356+Hsjoshi96@users.noreply.github.com>
2020-09-05 18:47:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+
+//   navbar style
+
+// document.addEventListener('scroll', function() {
+//     document.querySelector(".dis").classList.add("dis2");
+//   });
+
+// window.onscroll = function (e) {
+//     // print "false" if direction is down and "true" if up
+//     var dir;
+//     if (this.oldScroll < this.scrollY) {
+        
+//         document.querySelector(".dis").classList.remove("dis").add("dis2");
+        
+        
+
+//         } else {
+           
+//         }
+    
+        
+    
+//     console.log(this.oldScroll > this.scrollY)
+//     this.oldScroll = this.scrollY;
+    
+// }
+
